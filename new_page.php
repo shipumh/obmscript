55faf9adaaeb41f1cca480483dc45ab5ffc42f06
shipumh <shipu98@gmail.com>
2016-06-29 02:27:33 +0600

    Create new_page.php

---
+++ new_page.php
@@ -0,0 +1,35 @@
+<?php
+
+include(__DIR__ ."/../inc/header.php");
+
+/* Includes all necessary functionalities including database connection */
+
+if(!login_check()) 
+{ 
+header("Location: login.php");
+exit();
+} 
+
+/* redirect to login page if not logged in */
+
+/* Perform all queries here */
+
+$get_user = $con->query("SELECT * FROM `members`");  
+
+$page_title = "New Page";
+
+include(__DIR__ ."/../inc/head_struc.php");  
+
+/* head_struc.php is containing template header file   */
+?>
+<!-- Body contents -->
+<div id="container">
+
+<p>Hello world!</p>
+</div> 
+
+
+<?php
+include(__DIR__ ."/../inc/footer.php");
+
+/*  footer contents of template  */
